353f06675b3dde83d9669c568355341345ffc796
Yuma Andhika <137638903+yumaandhika@users.noreply.github.com>
2026-05-26 14:36:06 +0700

    fix(requisition-order): fixing mapping json api for list requisiton and order

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -36,25 +36,25 @@ class Order extends Equatable {
   factory Order.fromJson(Map<String, dynamic> json) {
     return Order(
       id: json['id']?.toString() ?? '',
-      createdAt: json['created_at'] != null
-          ? DateTime.tryParse(json['created_at'].toString())
+      createdAt: json['createdAt'] != null
+          ? DateTime.tryParse(json['createdAt'].toString())
           : null,
-      createdBy: json['created_by']?.toString(),
-      requisitionId: json['requisition_id']?.toString(),
+      createdBy: json['createdBy']?.toString(),
+      requisitionId: json['requisitionId']?.toString(),
       number: json['number']?.toString() ?? '',
-      issuerId: json['issuer_id']?.toString() ?? '',
-      issueDate: json['issue_date']?.toString() ?? '',
-      supplierId: json['supplier_id']?.toString() ?? '',
-      paymentTermId: json['payment_term_id']?.toString(),
-      deliveryTerm: json['delivery_term']?.toString(),
-      deliveryDate: json['delivery_date']?.toString(),
-      deliveryPointId: json['delivery_point_id'] is int
-          ? json['delivery_point_id']
-          : int.tryParse(json['delivery_point_id']?.toString() ?? ''),
+      issuerId: json['issuerId']?.toString() ?? '',
+      issueDate: json['issueDate']?.toString() ?? '',
+      supplierId: json['supplierId']?.toString() ?? '',
+      paymentTermId: json['paymentTermId']?.toString(),
+      deliveryTerm: json['deliveryTerm']?.toString(),
+      deliveryDate: json['deliveryDate']?.toString(),
+      deliveryPointId: json['deliveryPointId'] is int
+          ? json['deliveryPointId']
+          : int.tryParse(json['deliveryPointId']?.toString() ?? ''),
       remarks: json['remarks']?.toString(),
-      statusId: json['status_id'] is int
-          ? json['status_id']
-          : int.tryParse(json['status_id']?.toString() ?? '') ?? 1,
+      statusId: json['statusId'] is int
+          ? json['statusId']
+          : int.tryParse(json['statusId']?.toString() ?? '') ?? 1,
     );
   }
 
